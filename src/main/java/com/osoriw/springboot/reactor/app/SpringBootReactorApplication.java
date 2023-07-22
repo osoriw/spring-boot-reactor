e1b60a9449215d1a34247c8c955056400935b559
waosorio2021 <89051791+waosorio2021@users.noreply.github.com>
2023-07-22 14:50:07 -0500

    Imprimiendo logs en el método subscriber
    Imprimiendo logs en el método subscriber.

--- src/main/java/com/osoriw/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/osoriw/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,5 +1,7 @@
 package com.osoriw.springboot.reactor.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +11,8 @@ import reactor.core.publisher.Flux;
 @SpringBootApplication
 public class SpringBootReactorApplication implements CommandLineRunner {
 
+	private static final Logger log = LoggerFactory.getLogger(SpringBootReactorApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
 	}
@@ -17,11 +21,20 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 
 		// Ejemplo 1: creando un flujo reactivo
-		Flux<String> names = Flux.just("Andrés", "Rubén", "María", "Roberto", "Diego")
+		System.out.println("Creando un flujo reactivo:");
+		Flux<String> names1 = Flux.just("Andrés", "Rubén", "María", "Roberto", "Diego")
 				.doOnNext(name -> System.out.println(name));
 
-		names.subscribe();
+		names1.subscribe();
+		System.out.println("\n");
 
+		// Ejemplo 2: imprimiendo logs en el método subscriber
+		System.out.println("Imprimiendo logs en el método subscriber:");
+		Flux<String> names2 = Flux.just("Andrés", "Rubén", "María", "Roberto", "Diego")
+				.doOnNext(name -> System.out.println(name));
+
+		names2.subscribe(name -> log.info(name));
+		System.out.println("\n");
 	}
 
 }
