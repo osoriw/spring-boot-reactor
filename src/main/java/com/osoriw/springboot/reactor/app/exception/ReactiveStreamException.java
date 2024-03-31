27ba5d07c10ea1edabc2a21b0bba8db59ca087ad
waosorio2021 <89051791+waosorio2021@users.noreply.github.com>
2024-03-31 16:09:14 -0500

    Refactoring comments iin ReactiveStreamException.java
    Refactoring comments iin ReactiveStreamException.java

--- src/main/java/com/osoriw/springboot/reactor/app/exception/ReactiveStreamException.java
+++ src/main/java/com/osoriw/springboot/reactor/app/exception/ReactiveStreamException.java
@@ -4,17 +4,24 @@ import reactor.core.publisher.Flux;
 
 public class ReactiveStreamException {
 
+    /**
+     * Throws an exception when an error occurs
+     */
     public Flux<String> throwingAnException() {
         return Flux.just("A", "B", "C")
-                .concatWith(Flux.error(new RuntimeException("Exception Ocurred")))
+                .concatWith(Flux.error(new RuntimeException("An exception has occurred")))
                 .concatWith(Flux.just("D"))
                 .log();
     }
 
+    /**
+     * Return a default value when an exception occurs
+     */
     public Flux<String> catchingAnExceptionWithOnErrorReturn() {
         return Flux.just("A", "B", "C")
-                .concatWith(Flux.error(new RuntimeException("Exception Ocurred")))
-                .onErrorReturn("D")
+                .concatWith(Flux.error(new RuntimeException("An exception has occurred")))
+                .onErrorReturn("D") //Note that this method does not have visibility of the exception. it just returns a default value when the exception occurs
                 .log();
     }
+
 }
